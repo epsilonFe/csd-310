c75feabdc394f288af433c551d0f986ce8df83e8
Javier Enrique Mascareno Urribarri <63876199+epsilonFe@users.noreply.github.com>
2022-01-22 20:51:06 -0500

    Committing module_5

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,6 @@
+import pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.i5ype.mongodb.net/mongodb_test.py?retryWrites=true&w=majority"
+client = MongoClient("mongodb+srv://admin:admin@cluster0.i5ype.mongodb.net/mongodb_test.py?retryWrites=true&w=majority")
+db = client.pytech
+print(db.list_collection_names)
