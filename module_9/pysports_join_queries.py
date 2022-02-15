b6e74bc951c0037166a813479ea0502ab66b35a1
Javier Enrique Mascareno Urribarri <63876199+epsilonFe@users.noreply.github.com>
2022-02-14 21:34:23 -0500

    Commit error correction missing a player[2]

--- module_9/pysports_join_queries.py
+++ module_9/pysports_join_queries.py
@@ -20,14 +20,14 @@ try:
     print("\n -- DISPLAYING PLAYER RECORDS --")
 
     for player in players:
-        print("   Player ID: {}\n   First Name: {}\n   Last Name: {}\n   Team Name: {}\n".format(player[0], player[1], player[3]))
+        print("  Player ID: {}\n  First Name: {}\n  Last Name: {}\n  Team Name: {}\n".format(player[0], player[1], player[2], player[3]))
 
     input("\n\n  Press any key to continue...")
 
 except mysql.connector.Error as err:
 
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("   The supplied username or password are invalid")
+        print("  The supplied username or password are invalid")
 
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
         print("  The specified database does not exist")
