56182fbb1feaf33731d05e515664998dbc4c349d
Javier Enrique Mascareno Urribarri <63876199+epsilonFe@users.noreply.github.com>
2022-02-26 20:21:41 -0500

    Commit

--- module_11/whatabook_program_queries.sql
+++ module_11/whatabook_program_queries.sql
@@ -2,13 +2,12 @@ DROP USER IF EXISTS 'quebook_user'@'localhost';
 CREATE USER 'quebook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'XkuELbuck0';
 GRANT ALL PRIVILEGES ON quebook.* TO'quebook_user'@'localhost';
 
-USE quebook;
-
-SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author
 FROM wishlist
     INNER JOIN user ON wishlist.user_id = user.user_id
     INNER JOIN book ON wishlist.book_id = book.book_id
 WHERE user.user_id = 1;
+
+SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author
 
 SELECT store_id, locale from store;
 
