809bfc153a75ea0e61c82626e6037fc85cc29ec5
Javier Enrique Mascareno Urribarri <63876199+epsilonFe@users.noreply.github.com>
2022-02-26 20:55:09 -0500

    Commit 02/26/22

--- module_11/whatabook_program_queries.sql
+++ module_11/whatabook_program_queries.sql
@@ -1,13 +1,9 @@
-DROP USER IF EXISTS 'quebook_user'@'localhost';
-CREATE USER 'quebook_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'XkuELbuck0';
-GRANT ALL PRIVILEGES ON quebook.* TO'quebook_user'@'localhost';
+SELECT patrons.patron_id, patrons.first_name, patrons.last_name, book.book_id, book.book_name, book.author
 
 FROM wishlist
-    INNER JOIN user ON wishlist.user_id = user.user_id
+    INNER JOIN patrons ON wishlist.patron_id = patrons.patron_id
     INNER JOIN book ON wishlist.book_id = book.book_id
-WHERE user.user_id = 1;
-
-SELECT user.user_id, user.first_name, user.last_name, book.book_id, book.book_name, book.author
+WHERE patrons.patron_id = 1;
 
 SELECT store_id, locale from store;
 
@@ -15,9 +11,9 @@ SELECT book_id, book_name, author, book_year from book;
 
 SELECT book_id, book_name, author, book_year
 FROM book
-WHERE book_id IN (SELECT book_id FROM wishlist WHERE user_id = 1);
+WHERE book_id NOT IN (SELECT book_id FROM wishlist WHERE patron_id = 1);
 
-INSERT INTO wishlist(user_id, book_id)
-    VALUES(1, 9)
+INSERT INTO wishlist(patron_id, book_id)
+    VALUES(1, 9);
 
-DELETE FROM wishlist WHERE user_id = 1 AND book_id = 9;
+DELETE FROM wishlist WHERE patron_id = 1 AND book_id = 9;
